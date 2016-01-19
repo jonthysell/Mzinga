eaa82166e1bc050de8265e44f70209677d741309
Jon Thysell <thysell@gmail.com>
2016-01-18 18:14:08 -0800

    Added setup installer
    * Added new installer
    * Added new icon
    * Fixed issue with selectable move history

--- Mzinga.Viewer/Properties/AssemblyInfo.cs
+++ Mzinga.Viewer/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.16019.0027")]
-[assembly: AssemblyFileVersion("0.9.16019.0027")]
+[assembly: AssemblyVersion("0.9.16019.0210")]
+[assembly: AssemblyFileVersion("0.9.16019.0210")]
