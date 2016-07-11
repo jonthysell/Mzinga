c04c99d8fe79b807db16d5bce0612ccf91771622
Jon Thysell <thysell@gmail.com>
2016-07-11 08:53:11 -0700

    Version 0.9.16193.1550 release

--- Mzinga.Trainer/Properties/AssemblyInfo.cs
+++ Mzinga.Trainer/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("0.9.16193.1550")]
+[assembly: AssemblyFileVersion("0.9.16193.1550")]
--- Mzinga.Viewer/Properties/AssemblyInfo.cs
+++ Mzinga.Viewer/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.16189.0350")]
-[assembly: AssemblyFileVersion("0.9.16189.0350")]
+[assembly: AssemblyVersion("0.9.16193.1550")]
+[assembly: AssemblyFileVersion("0.9.16193.1550")]
