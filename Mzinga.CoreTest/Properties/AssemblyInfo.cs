da07c19cf6f7e284cbaf3e4d3a31a2ba34e59ba6
Jon Thysell <thysell@gmail.com>
2016-07-25 08:24:46 -0700

    Version 0.9.16207.1521 release

--- Mzinga.CoreTest/Properties/AssemblyInfo.cs
+++ Mzinga.CoreTest/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.16207.1449")]
-[assembly: AssemblyFileVersion("0.9.16207.1449")]
+[assembly: AssemblyVersion("0.9.16207.1521")]
+[assembly: AssemblyFileVersion("0.9.16207.1521")]
