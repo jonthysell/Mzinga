08c3b61300c58f194d430b2e7f8666fdcd4a7f4c
Jon Thysell <thysell@gmail.com>
2016-08-02 10:55:11 -0700

    Version 0.9.16215.1753 release

--- Mzinga.Viewer/Properties/AssemblyInfo.cs
+++ Mzinga.Viewer/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.16207.1525")]
-[assembly: AssemblyFileVersion("0.9.16207.1525")]
+[assembly: AssemblyVersion("0.9.16215.1753")]
+[assembly: AssemblyFileVersion("0.9.16215.1753")]
