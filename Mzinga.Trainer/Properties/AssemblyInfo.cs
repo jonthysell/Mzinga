fc080b2bd60c428793461bb785c4ac898041756b
Jon Thysell <thysell@gmail.com>
2016-12-29 17:56:14 -0800

    Fix automatic versioning
    * Fixes issue #47, now versioning is handled by a shared cs file that is
    generated by a T4 template instead of with a VS extension

--- Mzinga.Trainer/Properties/AssemblyInfo.cs
+++ Mzinga.Trainer/Properties/AssemblyInfo.cs
@@ -2,35 +2,9 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Mzinga.Trainer")]
 [assembly: AssemblyDescription("Trainer for Mzinga.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Jon Thysell")]
-[assembly: AssemblyProduct("Mzinga")]
 [assembly: AssemblyCopyright("Copyright © 2016 Jon Thysell")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7b0697cb-eca1-4d73-99a2-563dbd42400f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.16242.1557")]
-[assembly: AssemblyFileVersion("0.9.16242.1557")]
