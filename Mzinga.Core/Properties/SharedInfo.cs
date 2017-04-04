f75965555d543e058838c6acc7df368999cd9a54
Jon Thysell <thysell@gmail.com>
2017-04-04 07:33:20 -0700

    Version 0.9.17094.1431 release * Version bump

--- Mzinga.Core/Properties/SharedInfo.cs
+++ Mzinga.Core/Properties/SharedInfo.cs
@@ -9,5 +9,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.9.17092.1137")]
-[assembly: AssemblyFileVersion("0.9.17092.1137")]
+[assembly: AssemblyVersion("0.9.17094.1431")]
+[assembly: AssemblyFileVersion("0.9.17094.1431")]
