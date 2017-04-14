a17f93a7012d79e007420b1abe54c9d2f195ef42
Jon Thysell <thysell@gmail.com>
2017-04-14 08:07:45 -0700

    Mzinga v0.9.17104.1505 Release

--- Mzinga.Core/Properties/SharedInfo.cs
+++ Mzinga.Core/Properties/SharedInfo.cs
@@ -9,5 +9,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.9.17094.1431")]
-[assembly: AssemblyFileVersion("0.9.17094.1431")]
+[assembly: AssemblyVersion("0.9.17104.1505")]
+[assembly: AssemblyFileVersion("0.9.17104.1505")]
