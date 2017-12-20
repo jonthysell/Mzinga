816df76d19e0a711602259d4811836e05d69f753
Jon Thysell <thysell@gmail.com>
2017-12-20 12:33:21 -0800

    Version v0.9.17354.2030 release

--- Mzinga.Core/Properties/SharedInfo.cs
+++ Mzinga.Core/Properties/SharedInfo.cs
@@ -9,5 +9,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.9.17188.1808")]
-[assembly: AssemblyFileVersion("0.9.17188.1808")]
+[assembly: AssemblyVersion("0.9.17354.2030")]
+[assembly: AssemblyFileVersion("0.9.17354.2030")]
