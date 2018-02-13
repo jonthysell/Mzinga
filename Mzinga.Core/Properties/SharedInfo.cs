713e0ea817a558a1abb652fb3a98d9ee2dc87bab
Jon Thysell <thysell@gmail.com>
2018-02-12 20:31:07 -0800

    Version v0.9.18044.0429 release

--- Mzinga.Core/Properties/SharedInfo.cs
+++ Mzinga.Core/Properties/SharedInfo.cs
@@ -9,5 +9,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.9.18030.1801")]
-[assembly: AssemblyFileVersion("0.9.18030.1801")]
+[assembly: AssemblyVersion("0.9.18044.0429")]
+[assembly: AssemblyFileVersion("0.9.18044.0429")]
