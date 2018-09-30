25fa0e7707cc74271c32b283ff8619b295a3fef7
Jon Thysell <thysell@gmail.com>
2018-09-30 12:21:22 -0700

    Viewer: Adding info/error window sounds

--- Mzinga.Viewer/ExceptionWindow.xaml.cs
+++ Mzinga.Viewer/ExceptionWindow.xaml.cs
@@ -4,7 +4,7 @@
 // Author:
 //       Jon Thysell <thysell@gmail.com>
 // 
-// Copyright (c) 2015, 2017 Jon Thysell <http://jonthysell.com>
+// Copyright (c) 2015, 2017, 2018 Jon Thysell <http://jonthysell.com>
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -24,6 +24,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+using System.Media;
 using System.Windows;
 
 namespace Mzinga.Viewer
@@ -36,6 +37,11 @@ namespace Mzinga.Viewer
         public ExceptionWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SystemSounds.Exclamation.Play();
         }
     }
 }
