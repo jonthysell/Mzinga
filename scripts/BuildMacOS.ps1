8ae84fe793cce6f2798777527c20193275c39736
Jon Thysell <thysell@gmail.com>
2021-01-27 15:52:56 -0800

    Fix BuildLinux and BuildMacOS scripts to not build Mzinga.Viewer

--- scripts/BuildMacOS.ps1
+++ scripts/BuildMacOS.ps1
@@ -1,6 +1,6 @@
 [string] $Product = "Mzinga"
 [string] $Target = "MacOS"
 
-& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -BuildArgs "-target:Publish -p:RuntimeIdentifier=osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true"
+& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -BuildArgs "-target:Mzinga_Engine:Publish;Mzinga_Trainer:Publish;Mzinga_Perft:Publish -p:RuntimeIdentifier=osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true"
 
 & "$PSScriptRoot\TarRelease.ps1" -Product $Product -Target $Target
