afcc3ccd2597b6b3c6a9e281a249f1a3535ad31f
Jon Thysell <thysell@gmail.com>
2021-01-27 12:39:22 -0800

    Build: Added build scripts and workflows

---
+++ scripts/BuildMacOS.ps1
@@ -0,0 +1,6 @@
+[string] $Product = "Mzinga"
+[string] $Target = "MacOS"
+
+& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -BuildArgs "-target:Publish -p:RuntimeIdentifier=osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true"
+
+& "$PSScriptRoot\TarRelease.ps1" -Product $Product -Target $Target
