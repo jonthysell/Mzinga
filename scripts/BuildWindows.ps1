5ecaeac5e135ee281976c71ef19af050f8adaf6b
Jon Thysell <thysell@gmail.com>
2021-01-27 14:36:46 -0800

    Fix BuildWindows script to build x64, not x86

--- scripts/BuildWindows.ps1
+++ scripts/BuildWindows.ps1
@@ -3,6 +3,6 @@ param()
 [string] $Product = "Mzinga"
 [string] $Target = "Windows"
 
-& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -BuildArgs "-target:Publish -p:RuntimeIdentifier=win-x86 -p:PublishSingleFile=true -p:PublishTrimmed=true"
+& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -BuildArgs "-target:Publish -p:RuntimeIdentifier=win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true"
 
 & "$PSScriptRoot\ZipRelease.ps1" -Product $Product -Target $Target
