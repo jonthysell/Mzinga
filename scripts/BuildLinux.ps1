4ce531ed086241f60c8b1c67d034ae16c4a983b3
Jon Thysell <thysell@gmail.com>
2021-01-27 16:00:24 -0800

    Revert "Fix BuildLinux and BuildMacOS scripts to not build Mzinga.Viewer"
    This reverts commit 8ae84fe793cce6f2798777527c20193275c39736.

--- scripts/BuildLinux.ps1
+++ scripts/BuildLinux.ps1
@@ -3,6 +3,6 @@ param()
 [string] $Product = "Mzinga"
 [string] $Target = "Linux"
 
-& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -BuildArgs "-target:Mzinga_Engine:Publish;Mzinga_Trainer:Publish;Mzinga_Perft:Publish -p:RuntimeIdentifier=linux-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true"
+& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -BuildArgs "-target:Publish -p:RuntimeIdentifier=linux-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true"
 
 & "$PSScriptRoot\TarRelease.ps1" -Product $Product -Target $Target
