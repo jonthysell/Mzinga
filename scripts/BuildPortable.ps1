b20cb046939c6998dd2397a1b2be8426a8be42b2
Jon Thysell <thysell@gmail.com>
2021-01-27 17:27:53 -0800

    Added CliRelease configuration

--- scripts/BuildPortable.ps1
+++ scripts/BuildPortable.ps1
@@ -3,6 +3,6 @@ param()
 [string] $Product = "Mzinga"
 [string] $Target = "Portable"
 
-& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -BuildArgs "-target:Publish"
+& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -BuildArgs "-p:Configuration=Release -target:Publish"
 
 & "$PSScriptRoot\ZipRelease.ps1" -Product $Product -Target $Target
