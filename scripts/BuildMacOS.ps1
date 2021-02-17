f289fe7eecfc3526b56ec7ba6eeb1ba87b3e24ff
Jon Thysell <thysell@gmail.com>
2021-02-16 17:07:44 -0800

    Disable MacOS app bundle

--- scripts/BuildMacOS.ps1
+++ scripts/BuildMacOS.ps1
@@ -1,8 +1,9 @@
 [string] $Product = "Mzinga"
 [string] $Target = "MacOS"
 
-& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -BuildArgs "-t:BundleApp -p:RuntimeIdentifier=osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true" -ProjectPath "src\$Product.Viewer\$Product.Viewer.csproj"
+# Disabled creating the app bundle until crash is fixed: https://github.com/jonthysell/Mzinga/issues/100
+# & "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -BuildArgs "-t:BundleApp -p:RuntimeIdentifier=osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true" -ProjectPath "src\$Product.Viewer\$Product.Viewer.csproj"
 
-& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -BuildArgs "-t:Publish -p:RuntimeIdentifier=osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true" -Clean $False
+& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -BuildArgs "-t:Publish -p:RuntimeIdentifier=osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true"
 
 & "$PSScriptRoot\TarRelease.ps1" -Product $Product -Target $Target
