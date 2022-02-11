a3df6874d716f630773f6de70659c755995cd6bf
Jon Thysell <thysell@gmail.com>
2022-02-10 16:51:10 -0800

    Update ZipRelease to use Compress-Archive

--- scripts/ZipRelease.ps1
+++ scripts/ZipRelease.ps1
@@ -14,16 +14,8 @@ if (Test-Path "$OutputRoot\$TargetOutputPackageName") {
 
 Set-Location -Path "$OutputRoot"
 
-if (-Not (Test-Path ".\7za.exe")) {
-    Write-Host "Get 7za.exe..."
-    curl https://www.7-zip.org/a/7zr.exe -o 7zr.exe 
-    curl https://www.7-zip.org/a/7z1900-extra.7z -o 7z1900-extra.7z
-    .\7zr.exe e 7z1900-extra.7z -y x64\7za.exe
-    Remove-Item 7zr.exe
-    Remove-Item 7z1900-extra.7z
-}
-
 Write-Host "Create package..."
-.\7za.exe a "$TargetOutputPackageName" "$TargetOutputDirectory" -tzip -mx9
+
+Compress-Archive -Path "$TargetOutputDirectory" -DestinationPath "$TargetOutputPackageName"
 
 Set-Location -Path "$StartingLocation"
