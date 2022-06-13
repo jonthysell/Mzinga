4979dad4f034068e803af7a71705bce869d544af
Jon Thysell <thysell@gmail.com>
2022-06-12 22:09:24 -0700

    Added CreateReleaseNotes script

---
+++ scripts/CreateReleaseNotes.ps1
@@ -0,0 +1,33 @@
+param()
+
+[string] $RepoRoot = Resolve-Path "$PSScriptRoot\.."
+
+[string] $OutputRoot = "bld"
+
+$StartingLocation = Get-Location
+Set-Location -Path $RepoRoot
+
+Write-Host "Getting release notes..."
+try
+{
+    $ChangelogLines = Get-Content "CHANGELOG.md"
+
+    $LastReleaseHeaders = $ChangelogLines | Select-String -Pattern "^## .* ##$" | Select -First 2
+
+    $FirstIndex = [array]::IndexOf($ChangelogLines, $LastReleaseHeaders[0]) + 1
+    $LastIndex = [array]::IndexOf($ChangelogLines, $LastReleaseHeaders[1]) - 1
+
+    $ReleaseNotes = $ChangelogLines[$FirstIndex..$LastIndex] -Match "\*"
+
+    $ReleaseNotes | ForEach-Object { Write-Host $_ }
+
+    if (-not (Test-Path "$OutputRoot")) {
+        New-Item "$OutputRoot" -Type Directory | Out-Null
+    }
+
+    Set-Content "$OutputRoot\ReleaseNotes.md" $ReleaseNotes
+}
+finally
+{
+    Set-Location -Path "$StartingLocation"
+}
