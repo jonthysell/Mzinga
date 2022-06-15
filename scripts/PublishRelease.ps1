3c3a9be7fcbeda4e1ef4ec93486dab8670e6fcfa
Jon Thysell <thysell@gmail.com>
2022-06-15 13:00:33 -0700

    Add PublishRelease.ps1 script

---
+++ scripts/PublishRelease.ps1
@@ -0,0 +1,71 @@
+param(
+    [string]$BumpPart = "patch",
+    [string]$DirectoryBuildPropsFile = "src\Directory.Build.props",
+    [string]$AppxManifestFile = "src\Mzinga.Viewer.WinStore\Package.appxmanifest",
+    [string]$ChangelogFile = ".\CHANGELOG.md"
+)
+
+# Adapted from https://gist.github.com/derantell/b8a4c87ea50177b900379df92a25de9d
+function Bump-Version {
+    param(
+        [string]$Version,
+        [string]$BumpPart
+    )
+
+    $Version -Match "^(\d+)\.(\d+)\.(\d+)(.*)$" | Out-Null
+    $major, $minor, $patch, $rest = [int]$matches[1], [int]$matches[2], [int]$matches[3], $matches[4]
+
+    switch ($BumpPart) {
+        "major" { $major += 1; $minor = 0; $patch = 0 } 
+        "minor" { $minor += 1; $patch = 0 }
+        "patch" { $patch += 1 }
+    }
+
+    return "$major.$minor.$patch$rest"
+}
+
+[string] $RepoRoot = Resolve-Path "$PSScriptRoot\.."
+
+[string] $OutputRoot = "bld"
+
+$StartingLocation = Get-Location
+Set-Location -Path $RepoRoot
+
+Write-Host "Building release commit for publish..."
+try
+{
+    [xml]$DirectoryBuildProps = Get-Content $DirectoryBuildPropsFile
+
+    $CurrentVersion = $DirectoryBuildProps.Project.PropertyGroup.Version
+
+    Write-Host "Current version: $CurrentVersion"
+
+    $NewVersion = Bump-Version $CurrentVersion $BumpPart
+
+    Write-Host "New version: $NewVersion"
+
+    Write-Host "Updating $DirectoryBuildPropsFile with new version..."
+    (Get-Content $DirectoryBuildPropsFile).Replace($CurrentVersion, $NewVersion) | Set-Content $DirectoryBuildPropsFile
+
+    Write-Host "Updating $AppxManifestFile with new version..."
+    (Get-Content $AppxManifestFile).Replace($CurrentVersion, $NewVersion) | Set-Content $AppxManifestFile -Encoding 'utf8BOM'
+
+    Write-Host "Updating $ChangelogFile with new version..."
+    (Get-Content $ChangelogFile).Replace("## next ##", "## v$NewVersion ##") | Set-Content $ChangelogFile
+
+    Write-Host "Creating release commit..."
+    &git commit -a -m "Version v$NewVersion release"
+
+    Write-Host "Tagging release commit..."
+    &git tag v$NewVersion
+
+    Write-Host "Pushing release commit..."
+    &git push
+
+    Write-Host "Pushing tags..."
+    &git push --tags
+}
+finally
+{
+    Set-Location -Path "$StartingLocation"
+}
